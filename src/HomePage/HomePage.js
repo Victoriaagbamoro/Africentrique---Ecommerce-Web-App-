6de480f1c3da263ba1c2e6a6a29a560c1aba1b35
Agbamoro Victoria <45354743+vickyagba@users.noreply.github.com>
2020-07-13 14:46:28 +0100

    ECommerce

---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './HomePage.scss'
+
+export default function HomePage(){
+    return(
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">ANKARA TOPS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JUMP SUIT</h1>
+                        <span className="subtitle">SHOP NOW</span>
+
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">ANKI BAGS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">ANKI TOPS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">SKIRTS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+
+                    </div>
+                </div>
+                
+
+            </div>
+        </div>
+    )
+}
